1cccaa4856a55e62dc47e8049ee1f2db4a354798
Utkarsh <u4utkarshraj@gmail.com>
2025-04-02 22:34:27 +0530

    till useState

---
+++ React/usestate-hook-practice-forked/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  const now = new Date().toLocaleTimeString();
+  const [time, setTime] = React.useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  setInterval(updateTime, 1000);
+
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
